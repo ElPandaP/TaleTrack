bb67f81bd386456478dc4458ca2212f8257a3d28
ElPandaP <pelayo.garcia.varela@gmail.com>
2026-02-20 03:13:33 +0100

    model changes

--- MediaTrackerApp/Model/Media.cs
+++ MediaTrackerApp/Model/Media.cs
@@ -1,10 +1,39 @@
 namespace MediaTrackerApp.Model;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Media
 {
+    [Key]
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(255, MinimumLength = 1, ErrorMessage = "Title must be between 1 and 255 characters")]
     public required string Title { get; set; }
-    public required string Type { get; set; }  // "Movie", "Series", "Book"
-    public int length { get; set; } // Length in minutes/pages/episodes
+    
+    [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Type is required")]
+    [StringLength(20, ErrorMessage = "Type cannot exceed 20 characters")]
+    [RegularExpression(@"^(Movie|Series|Book)$", ErrorMessage = "Type must be 'Movie', 'Series', or 'Book'")]
+    public required string Type { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Length must be greater than 0")]
+    public int Length { get; set; }
+    
+    [StringLength(500, ErrorMessage = "External ID cannot exceed 500 characters")]
+    public string? ExternalId { get; set; }
+    
+    [Url(ErrorMessage = "Invalid URL format for PosterUrl")]
+    [StringLength(2048, ErrorMessage = "PosterUrl cannot exceed 2048 characters")]
+    public string? PosterUrl { get; set; }
+    
+    [Required]
     public DateTime FirstTrackedAt { get; set; } = DateTime.UtcNow;
+    
+    public DateTime? UpdatedAt { get; set; }
+    
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    public ICollection<TrackingEvent> TrackingEvents { get; set; } = new List<TrackingEvent>();
 }
--- MediaTrackerApp/Model/User.cs
+++ MediaTrackerApp/Model/User.cs
@@ -1,11 +1,40 @@
 namespace MediaTrackerApp.Model;
 
+using System.ComponentModel.DataAnnotations;
+
 public class User
 {
-    public int Id {get;set;}
-    public required string Email {get; set;}
-    public required string Username {get; set;}
-    public required string PasswordHash {get; set;}
-    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    [StringLength(256, ErrorMessage = "Email cannot exceed 256 characters")]
+    public required string Email { get; set; }
+    
+    [Required(ErrorMessage = "Username is required")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+    public required string Username { get; set; }
+    
+    [Required(ErrorMessage = "Password hash is required")]
+    [StringLength(512, ErrorMessage = "Password hash cannot exceed 512 characters")]
+    public required string PasswordHash { get; set; }
+    
+    [StringLength(512, ErrorMessage = "Refresh token cannot exceed 512 characters")]
+    public string? RefreshToken { get; set; }
+    
+    public DateTime? RefreshTokenExpiry { get; set; }
+    
+    public DateTime? LastLogin { get; set; }
+    
+    [Required]
+    public bool IsActive { get; set; } = true;
+    
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public DateTime? UpdatedAt { get; set; }
+    
     public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    public ICollection<TrackingEvent> TrackingEvents { get; set; } = new List<TrackingEvent>();
 }
